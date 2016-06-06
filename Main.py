09f8bebeb202e009ea4fea44af2f835663214600
TMaster <github321>
2016-06-06 19:32:57 +0300

    Creating a basic Marker

---
+++ Main.py
@@ -0,0 +1,5 @@
+from Marker import Marker
+
+marker = Marker(["Tomer","Khalili"])
+
+marker.print_marks();
---
+++ Marker.py
@@ -0,0 +1,6 @@
+class Marker:
+	def __init__(self,marks):
+		self.marks = marks
+
+	def print_marks(self):
+		print "marks: " + str(self.marks)
