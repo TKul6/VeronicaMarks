0ab3529da87ae841c6889a6792fc16efaf1dc012
TMaster <github321>
2016-06-24 13:45:32 +0300

    Iterating all files in the directory

--- Marker.py
+++ Marker.py
@@ -5,12 +5,17 @@ class Marker:
 		self.marks = marks
 
 	def mark_file(self,path):
+		print "	File Name:" + path
 		
-
 	def mark_directory(self,path):
-		
-
-	
-
-
+		if(os.path.isdir(path)):
+			for entry in os.listdir(path):
+				childPath = os.path.join(path,entry)
+				if(os.path.isdir(childPath)):
+					print "Directory: " + path;
+					self.mark_directory(childPath)
+				else:
+					self.mark_file(childPath);
+		else:
+			print "Direcotry " + path + " does not exsits";
 
